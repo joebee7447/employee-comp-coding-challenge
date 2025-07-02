054544a1bd5f7b776536c08c9350151326a3dc2c
joebee7447 <jtbumba@gmail.com>
2025-07-01 22:38:04 -0400

    Completed Development on Employee info, reporting, and compensation

--- src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.mindex.challenge.service.impl;
 
+import com.mindex.challenge.dao.CompensationRepository;
 import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +19,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+    @Autowired
+    private CompensationRepository compensationRepository;
 
     @Override
     public Employee create(Employee employee) {
@@ -29,7 +34,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee read(String id) {
-        LOG.debug("Creating employee with id [{}]", id);
+        //updated logger message, previously said 'creating'
+        LOG.debug("Reading employee with id [{}]", id);
 
         Employee employee = employeeRepository.findByEmployeeId(id);
 
@@ -44,6 +50,95 @@ public class EmployeeServiceImpl implements EmployeeService {
     public Employee update(Employee employee) {
         LOG.debug("Updating employee [{}]", employee);
 
+        //added a delete method, was getting an error on updating then reading from same ID on the existing code
+        //new functionality, it  deletes the old employee from the DB, then adds in the new one
+        employeeRepository.deleteByEmployeeId(employee.getEmployeeId());
+
         return employeeRepository.save(employee);
+    }
+
+    @Override
+    public ReportingStructure reports(String id) {
+        LOG.debug("Generating number of reports for employee with id [{}]", id);
+
+        Employee reportingEmployee = employeeRepository.findByEmployeeId(id);
+
+        if (reportingEmployee == null) {
+            throw new RuntimeException("Invalid employeeId: " + id);
+        }
+
+        //assigning the generated number of reports to its own method, due to utilizing recursion to increment effectively
+        ReportingStructure reportingStructure = new ReportingStructure(reportingEmployee, generateNumberOfReports(reportingEmployee, 0));
+
+        return reportingStructure;
+    }
+
+    @Override
+    public Compensation submitCompensation(String id, Compensation compensation) {
+        LOG.debug("Creating compensation for employee with id [{}]", id);
+
+        //Made the decision to migrate all compensation info to its own table/repository. this is because it is handled entirely separately/isnt dependent on any other employee info
+        //the compensation set/update method written here would be rendered obsolete, because we could just use the standard /update method to update the compensation info with the new employee payload
+        //using this way, we completely isolate all of the compensation info to its own space, rather than adding the compensation to the existing Employee object
+        //as if there are 2 tables in the DB for compensation and employee data each, and the employee id is the foreign key
+
+        compensation.setEmployeeCompensationId(id);
+
+        //checking to see if employee exists before creating/updating compensation data
+        if (employeeRepository.findByEmployeeId(id) == null) {
+            throw new RuntimeException("Invalid employeeId: " + id);
+        }
+
+        Compensation employeeCompensation = compensationRepository.findByEmployeeCompensationId(id);
+
+        //if compensation info does not exist for this user, create it
+        if (employeeCompensation == null) {
+            return compensationRepository.insert(compensation);
+        }
+
+        //if it exists, update the info. I am using the updated /update method that contains the fix mentioned above in the method
+        compensationRepository.deleteByEmployeeCompensationId(compensation.getEmployeeCompensationId());
+
+        return compensationRepository.save(compensation);
+
+    }
+
+    @Override
+    public Compensation readCompensation(String id) {
+        LOG.debug("Reading compensation for employee with id [{}]", id);
+
+        //checking to see if employee exists, returns invalid employee message
+        if (employeeRepository.findByEmployeeId(id) == null) {
+            throw new RuntimeException("Invalid employeeId: " + id);
+        }
+
+        Compensation employeeCompensation = compensationRepository.findByEmployeeCompensationId(id);
+
+        //checking to see if employee compensation exists, returns no compensation found for this employee message
+        if (employeeCompensation == null) {
+            throw new RuntimeException("No compensation data found for employeeId: " + id);
+        }
+
+        return employeeCompensation;
+    }
+
+    private Integer generateNumberOfReports(Employee employee, Integer numberOfReports) {
+
+        //once we reach an employee with no direct reports, we return out of the recursion
+        if(employee.getDirectReports() == null) {
+            return numberOfReports;
+        }
+
+        for(int i = 0; i < employee.getDirectReports().size(); i++) {
+
+            //in the existing database, the directReport employee objects are blank, only containing employee ID
+            //by doing this call below, we can fill in those extra variables, most notably, the direct reports object within employee
+            employee.getDirectReports().set(i, employeeRepository.findByEmployeeId(employee.getDirectReports().get(i).getEmployeeId()));
+
+            //increment the number of reports value and then recursively call the generateNumberOfReports method with incremented value, and each directReport employee
+            numberOfReports++;
+            numberOfReports = generateNumberOfReports(employee.getDirectReports().get(i), numberOfReports);
+        }
+        return numberOfReports;
     }
 }
--- src/test/java/com/mindex/challenge/service/impl/EmployeeServiceImplTest.java
+++ src/test/java/com/mindex/challenge/service/impl/EmployeeServiceImplTest.java
@@ -1,10 +1,18 @@
 package com.mindex.challenge.service.impl;
 
+import com.mindex.challenge.dao.CompensationRepository;
+import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -15,8 +23,14 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.ArgumentMatchers.any;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -24,9 +38,20 @@ public class EmployeeServiceImplTest {
 
     private String employeeUrl;
     private String employeeIdUrl;
+    private String employeeReportingStructureUrl;
 
+    @Mock
+    private EmployeeRepository mockEmployeeRepository;
+    @Mock
+    private CompensationRepository mockCompensationRepository;
+
+    //didn't remove existing test service
     @Autowired
     private EmployeeService employeeService;
+
+    //added additional one with mockito included
+    @InjectMocks
+    private EmployeeService testEmployeeService = new EmployeeServiceImpl();
 
     @LocalServerPort
     private int port;
@@ -38,6 +63,13 @@ public class EmployeeServiceImplTest {
     public void setup() {
         employeeUrl = "http://localhost:" + port + "/employee";
         employeeIdUrl = "http://localhost:" + port + "/employee/{id}";
+        employeeReportingStructureUrl = "http://localhost:" + port + "/reporting/{id}";
+
+        //utilizing mockito for compensation implementation
+        mockEmployeeRepository = Mockito.mock(EmployeeRepository.class);
+        mockCompensationRepository = Mockito.mock(CompensationRepository.class);
+
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
@@ -77,10 +109,112 @@ public class EmployeeServiceImplTest {
         assertEmployeeEquivalence(readEmployee, updatedEmployee);
     }
 
+    @Test(expected = RuntimeException.class)
+    public void testReadEmployeeNotFound() {
+        testEmployeeService.read("1");
+    }
+
+    @Test
+    public void testReports () {
+
+        //create 3 test employees and link together using direct reports
+        Employee testEmployee1 = new Employee();
+
+        Employee testEmployee2 = new Employee();
+        List<Employee> testEmployee2List = new ArrayList<>();
+        testEmployee2List.add(restTemplate.postForEntity(employeeUrl, testEmployee1, Employee.class).getBody());
+        testEmployee2.setDirectReports(testEmployee2List);
+
+        Employee testEmployee3 = new Employee();
+        List<Employee> testEmployee3List = new ArrayList<>();
+        testEmployee3List.add(restTemplate.postForEntity(employeeUrl, testEmployee2, Employee.class).getBody());
+        testEmployee3.setDirectReports(testEmployee3List);
+
+        //grab test id when posting the parent employee to the db
+        String testId = restTemplate.postForEntity(employeeUrl, testEmployee3, Employee.class).getBody().getEmployeeId();
+
+        //get reporting structure based on the test id grabbed above
+        ReportingStructure test = restTemplate.getForEntity(employeeReportingStructureUrl, ReportingStructure.class, testId).getBody();
+
+        //test for null value first, then expected value
+        assertNotNull(test);
+        assertEquals("2", test.getNumberOfReports().toString());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testReportsEmployeeNotFound() {
+        testEmployeeService.reports("1");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testSubmitCompensationEmployeeNotFound() {
+        Compensation testCompensation = createTestCompensation();
+        testEmployeeService.submitCompensation("1", testCompensation);
+    }
+
+    @Test
+    public void testSubmitCompensationCreateAndUpdate() {
+        Compensation testCompensation = createTestCompensation();
+        Mockito.when(mockEmployeeRepository.findByEmployeeId(any())).thenReturn(new Employee());
+        Mockito.when(mockCompensationRepository.insert(any(Compensation.class))).thenReturn(createTestCompensation());
+        Compensation testInserted = testEmployeeService.submitCompensation("12345", testCompensation);
+
+        assertNotNull(testInserted);
+        assertEquals("12345", testInserted.getEmployeeCompensationId());
+        assertEquals(testCompensation.getEffectiveDate(), testInserted.getEffectiveDate());
+        assertEquals(testCompensation.getSalary(), testInserted.getSalary());
+        Mockito.verify(mockCompensationRepository, Mockito.times(1)).insert(any(Compensation.class));
+
+        Compensation updatedCompensation = createTestCompensation();
+        updatedCompensation.setSalary("987654321");
+        updatedCompensation.setEmployeeCompensationId("54321");
+
+        Mockito.when(mockCompensationRepository.findByEmployeeCompensationId(any(String.class))).thenReturn(updatedCompensation);
+        Mockito.when(mockCompensationRepository.save(any(Compensation.class))).thenReturn(updatedCompensation);
+        Compensation testUpdated = testEmployeeService.submitCompensation("54321", updatedCompensation);
+
+        assertNotNull(testUpdated);
+        assertNotEquals(testUpdated.getSalary(), testCompensation.getSalary());
+        Mockito.verify(mockCompensationRepository, Mockito.times(1)).deleteByEmployeeCompensationId(any(String.class));
+        Mockito.verify(mockCompensationRepository, Mockito.times(1)).save(any(Compensation.class));
+    }
+
+    @Test
+    public void testReadCompensation() {
+        Mockito.when(mockEmployeeRepository.findByEmployeeId(any())).thenReturn(new Employee());
+        Mockito.when(mockCompensationRepository.findByEmployeeCompensationId(any())).thenReturn(createTestCompensation());
+
+        Compensation test = testEmployeeService.readCompensation("1");
+
+        assertNotNull(test);
+        assertEquals("12345", test.getEmployeeCompensationId());
+        assertEquals("$123,456.30", test.getSalary());
+        assertEquals(LocalDate.parse("2025-08-24"), test.getEffectiveDate());
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testReadCompensationEmployeeNotFound() {
+        testEmployeeService.readCompensation("1");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testReadCompensationDataNotFound() {
+        Mockito.when(mockEmployeeRepository.findByEmployeeId(any())).thenReturn(new Employee());
+        testEmployeeService.readCompensation("1");
+    }
+
     private static void assertEmployeeEquivalence(Employee expected, Employee actual) {
         assertEquals(expected.getFirstName(), actual.getFirstName());
         assertEquals(expected.getLastName(), actual.getLastName());
         assertEquals(expected.getDepartment(), actual.getDepartment());
         assertEquals(expected.getPosition(), actual.getPosition());
+    }
+
+    private static Compensation createTestCompensation() {
+        Compensation testCompensation = new Compensation();
+        testCompensation.setEmployeeCompensationId("12345");
+        testCompensation.setSalary("123456.3");
+        testCompensation.setEffectiveDate(LocalDate.parse("2025-08-24"));
+        return testCompensation;
     }
 }
